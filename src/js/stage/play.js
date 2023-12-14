06afa34d43d26e7675c7f9fff856f305123c0e6c
YanaPolshyna <112593332+YanaPolshyna@users.noreply.github.com>
2023-12-13 22:42:12 -0800

    Update play.js

--- src/js/stage/play.js
+++ src/js/stage/play.js
@@ -1,22 +1,33 @@
-import { Stage, game, ColorLayer, BitmapText  } from "melonjs";
+import * as me from 'melonjs/dist/melonjs.module.js';
 
-class PlayScreen extends Stage {
-    /**
-     *  action to perform on state change
-     */
-    onResetEvent() {
-        // add a gray background to the default Stage
-        game.world.addChild(new ColorLayer("background", "#202020"));
+import HUD from '../renderables/hud/container.js'
+import data from '../data.js'
 
-        // add a font text display object
-        game.world.addChild(new BitmapText(game.viewport.width / 2, game.viewport.height / 2,  {
-            font : "PressStart2P",
-            size : 4.0,
-            textBaseline : "middle",
-            textAlign : "center",
-            text : "Hello World !"
-        }));
-    }
-};
+export default class PlayScreen extends me.Stage {
 
-export default PlayScreen;
+	/**
+	 *  action to perform on state change
+	 */
+	onResetEvent() {
+
+		// load a level
+		me.level.load("area01");
+
+		// reset the score
+		data.score = 0;
+
+		// add our HUD to the game world
+		this.HUD = new HUDContainer();
+		me.game.world.addChild(this.HUD);
+
+	}
+
+
+	/**
+	 *  action to perform when leaving this screen (state change)
+	 */
+	onDestroyEvent() {
+		// remove the HUD from the game world
+		me.game.world.removeChild(this.HUD);
+	}
+}
