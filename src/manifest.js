5f4011f1fbfe179264c552e34ae2d11ebbd035b7
YanaPolshyna <112593332+YanaPolshyna@users.noreply.github.com>
2023-12-13 22:40:51 -0800

    Update manifest.js

--- src/manifest.js
+++ src/manifest.js
@@ -11,6 +11,17 @@ const DataManifest = [
         name: "PressStart2P",
         type: "binary",
         src: "./data/fnt/PressStart2P.fnt"
+    },
+
+    {
+    name: "area01_level_tiles",
+    type:"image",	
+    src: "data/img/map/area01_level_tiles.png"
+    },
+    {
+    name: "area01",           
+    type: "tmx",	
+    src: "data/map/area01.tmx"
     }
 ];
 
